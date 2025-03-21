a96455736b6ec67f8c28cf49e85c5dfc463236a4
Chris Forsythe <chrisforsythewebdev@gmail.com>
2025-03-21 14:48:38 +0000

    Initial commit - set up React with Tailwind CSS

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      
+      <div className="flex-grow page-content">
+      <h1 className="text-3xl font-bold underline">
+      hello</h1>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
